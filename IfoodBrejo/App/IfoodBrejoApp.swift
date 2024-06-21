158666de5904746d69c3473d629b1f6cb9722142
diogenesricardo <diogenesr.freitas@gmail.com>
2024-06-21 17:12:17 -0300

    commit inicial das telas

---
+++ IfoodBrejo/App/IfoodBrejoApp.swift
@@ -0,0 +1,17 @@
+//
+//  IfoodBrejoApp.swift
+//  IfoodBrejo
+//
+//  Created by Diogenes Ricardo Freitas de Oliveira on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct IfoodBrejoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
